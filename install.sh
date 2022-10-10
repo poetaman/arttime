47dddaca84b1355c1c40b198ea9aca91d92a615a
poetaman <githubaman@gmail.com>
2022-10-09 17:37:47 -0700

    Fix installer for bash systems, so users can start using arttime from other non-login interactive shells right away without having to re-login. After a login they are welcome to remove or move the line added to bottom of .bashrc elsewhere.

--- install.sh
+++ install.sh
@@ -179,13 +179,12 @@ else
     if [[ $loginshell == *zsh* ]]; then
         profile='.zshrc'
     elif [[ $loginshell == *bash* ]]; then
-        if [[ -e $HOME/.bash_profile ]]; then
-            profile='.bash_profile'
-        else
-            profile='.profile'
-        fi
-    elif [[ $loginshell == *ksh* ]]; then
-        profile='.profile'
+        #if [[ -e $HOME/.bash_profile ]]; then
+        #    profile='.bash_profile'
+        #else
+        #    profile='.profile'
+        #fi
+        profile=".bashrc"
     else
         profile=''
     fi
@@ -193,9 +192,10 @@ else
         echo "\n# Following line was automatically added by arttime installer" >>$HOME/$profile
         echo 'export PATH='"$bindir"':$PATH' >>$HOME/$profile
         echo '\nNote: Added export PATH='"$bindir"':$PATH to ~/'"$profile"
+        echo "      Feel free to move or remove it after next login/reboot to a more appropriate location."
         echo "Installation complete!\nRestart your terminal application, type 'arttime' and press Enter."
     else
-        echo "\nInstallation [31m*[0malmost[31m*[0m complete! To start using arttime, follow these steps:\n    1) Add $bindir to your PATH environment variable,\n    2) Restart your terminal application, type 'arttime' and press Enter."
+        echo "\nInstallation [31m*[0malmost[31m*[0m complete! To start using arttime, follow these steps:\n    1) Add $bindir to your PATH environment variable in appropriate file,\n    2) Open a new terminal session, type 'arttime' and press Enter."
     fi
 fi
 tput cnorm
